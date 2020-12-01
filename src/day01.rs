69669037a824bdfebfaf5684cde211f2b7813b72
Peter Helbing <peter@abulafia.org>
2020-12-01 07:38:21 +0100

    Day 01 (both stars)

--- src/day01.rs
+++ src/day01.rs
@@ -3,13 +3,35 @@ use crate::day::Day;
 pub struct Day01 {}
 
 impl Day for Day01 {
-    fn star1(&self, _input: &str) -> String {
-        String::from("not implemented")
+    fn star1(&self, input: &str) -> String {
+        let entries = parse_input(input);
+        for i in 0..entries.len() {
+            for j in i + 1..entries.len() {
+                if entries[i] + entries[j] == 2020 {
+                    return format!("{}", entries[i] * entries[j]);
+                }
+            }
+        }
+        String::from("not found")
     }
 
-    fn star2(&self, _input: &str) -> String {
-        String::from("not implemented")
+    fn star2(&self, input: &str) -> String {
+        let entries = parse_input(input);
+        for i in 0..entries.len() {
+            for j in i + 1..entries.len() {
+                for k in j + 1..entries.len() {
+                    if entries[i] + entries[j] + entries[k] == 2020 {
+                        return format!("{}", entries[i] * entries[j] * entries[k]);
+                    }
+                }
+            }
+        }
+        String::from("not found")
     }
+}
+
+fn parse_input(input: &str) -> Vec<u64> {
+    input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
 #[cfg(test)]
@@ -19,6 +41,13 @@ mod tests {
     #[test]
     fn ex1() {
         let d = Day01 {};
-        assert_eq!(d.star1(""), "not implemented");
+        let input = r#"1721
+979
+366
+299
+675
+1456"#;
+        assert_eq!(d.star1(input), "514579");
+        assert_eq!(d.star2(input), "241861950");
     }
 }
