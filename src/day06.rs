0d25e491b354b419b789b131e40215df8fdf4e6c
Peter Helbing <peter@abulafia.org>
2020-12-06 11:55:50 +0100

    Day 06, part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,25 +1,38 @@
 use crate::day::Day;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day06 {}
 
 impl Day for Day06 {
     fn star1(&self, input: &str) -> String {
         let groups = input.split("\n\n");
-        let sum_counts = groups
-            .map(|g| questions_per_group(g))
-            .fold(0, |sum_counts, q_set| sum_counts + q_set.len());
+        let sum_counts: usize = groups.map(|g| any_questions_per_group(g)).sum();
         format!("{}", sum_counts)
     }
 
-    fn star2(&self, _input: &str) -> String {
-        String::from("not implemented")
+    fn star2(&self, input: &str) -> String {
+        let groups = input.split("\n\n");
+        let sum_counts: usize = groups.map(|g| all_questions_per_group(g)).sum();
+        format!("{}", sum_counts)
     }
 }
 
-fn questions_per_group(group_str: &str) -> HashSet<char> {
+fn any_questions_per_group(group_str: &str) -> usize {
     let questions: HashSet<_> = group_str.chars().filter(|c| !c.is_whitespace()).collect();
-    questions
+    questions.len()
+}
+
+fn all_questions_per_group(group_str: &str) -> usize {
+    let lines: Vec<_> = group_str.lines().collect();
+    let mut answers = HashMap::new();
+    for line in &lines {
+        for ch in line.chars() {
+            let counter = answers.entry(ch).or_insert(0);
+            *counter += 1;
+        }
+    }
+    let all_answers: Vec<_> = answers.values().filter(|&v| *v == lines.len()).collect();
+    all_answers.len()
 }
 
 #[cfg(test)]
@@ -28,8 +41,7 @@ mod tests {
 
     #[test]
     fn ex1() {
-        let input = r#"
-abc
+        let input = r#"abc
 
 a
 b
@@ -47,5 +59,6 @@ b
 "#;
         let d = Day06 {};
         assert_eq!(d.star1(input), "11");
+        assert_eq!(d.star2(input), "6");
     }
 }
