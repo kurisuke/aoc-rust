63a230312e1b178be449197a0691410d44d5eed5
Peter Helbing <peter@abulafia.org>
2020-12-10 20:34:37 +0100

    Day 10

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod day06;
 mod day07;
 mod day08;
 mod day09;
+mod day10;
 mod grid2d;
 
 use day::Day;
@@ -25,6 +26,7 @@ fn get_day(day_no: usize) -> Option<Box<dyn Day>> {
         7 => Some(Box::new(day07::Day07 {})),
         8 => Some(Box::new(day08::Day08 {})),
         9 => Some(Box::new(day09::Day09 {})),
+        10 => Some(Box::new(day10::Day10 {})),
         _ => None,
     }
 }
