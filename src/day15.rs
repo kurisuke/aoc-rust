6ce9d3d61b82d137c10ebf0177f84372e2997c1d
Peter Helbing <peter@abulafia.org>
2020-12-16 12:55:11 +0100

    Day 15: speedup by using u32 elements

--- src/day15.rs
+++ src/day15.rs
@@ -2,24 +2,22 @@ use crate::day::Day;
 
 pub struct Day15 {}
 
-fn find_num(start_nums: &[usize], n: usize) -> usize {
-    let mut prev_turn = vec![0; n];
+fn find_num(start_nums: &[u32], n: u32) -> u32 {
+    let mut prev_turn = vec![0; n as usize];
 
     for (i, n) in start_nums.iter().enumerate().take(start_nums.len() - 1) {
-        prev_turn[*n] = i + 1;
+        prev_turn[*n as usize] = i as u32 + 1;
     }
     let mut last_spoken = *start_nums.last().unwrap();
 
-    for turn in start_nums.len()..n {
-        let next_spoken = if prev_turn[last_spoken] == 0 {
+    for turn in (start_nums.len() as u32)..n {
+        let next_spoken = if prev_turn[last_spoken as usize] == 0 {
             0
         } else {
-            turn - prev_turn[last_spoken]
+            turn - prev_turn[last_spoken as usize]
         };
 
-        prev_turn[last_spoken] = turn;
-
-        // next turn
+        prev_turn[last_spoken as usize] = turn;
         last_spoken = next_spoken;
     }
 
@@ -30,7 +28,7 @@ impl Day for Day15 {
     fn star1(&self, input: &str) -> String {
         let start_nums: Vec<_> = input
             .split(',')
-            .map(|x| x.parse::<usize>().unwrap())
+            .map(|x| x.parse::<u32>().unwrap())
             .collect();
         format!("{}", find_num(&start_nums, 2020))
     }
@@ -38,7 +36,7 @@ impl Day for Day15 {
     fn star2(&self, input: &str) -> String {
         let start_nums: Vec<_> = input
             .split(',')
-            .map(|x| x.parse::<usize>().unwrap())
+            .map(|x| x.parse::<u32>().unwrap())
             .collect();
         format!("{}", find_num(&start_nums, 30000000))
     }
