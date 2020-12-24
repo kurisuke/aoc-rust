ea88ee391c4cd5ccd3ba1c43a8ed730b5587467d
Peter Helbing <peter@abulafia.org>
2020-12-24 18:23:34 +0100

    Year 2015, Day 03

--- src/year2015/mod.rs
+++ src/year2015/mod.rs
@@ -3,6 +3,7 @@ use crate::year::Year;
 
 mod day01;
 mod day02;
+mod day03;
 
 pub struct Year2015 {}
 
@@ -11,6 +12,7 @@ impl Year for Year2015 {
         match day_no {
             1 => Some(Box::new(day01::Day01 {})),
             2 => Some(Box::new(day02::Day02 {})),
+            3 => Some(Box::new(day03::Day03 {})),
             _ => None,
         }
     }
