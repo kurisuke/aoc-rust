d7e437c01feda1ec128b9f7a3b37594105f49384
Peter Helbing <peter@abulafia.org>
2020-12-29 14:47:16 +0100

    Year 2016, Day 02

--- src/year2016/mod.rs
+++ src/year2016/mod.rs
@@ -2,6 +2,7 @@ use crate::day::Day;
 use crate::year::Year;
 
 mod day01;
+mod day02;
 
 pub struct Year2016 {}
 
@@ -9,6 +10,7 @@ impl Year for Year2016 {
     fn get_day(&self, day_no: usize) -> Option<Box<dyn Day>> {
         match day_no {
             1 => Some(Box::new(day01::Day01 {})),
+            2 => Some(Box::new(day02::Day02 {})),
             _ => None,
         }
     }
