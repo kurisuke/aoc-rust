3f21abb839fad3eb146032899562745dfbac4f43
Peter Helbing <peter@abulafia.org>
2021-04-16 12:57:56 +0200

    Year 2018, Day 10 & 12

--- year2018/src/day10.rs
+++ year2018/src/day10.rs
@@ -1,24 +1,96 @@
 use common::day::Day;
+use std::collections::HashSet;
+
+type CoordsXy = (isize, isize);
+
+struct Point {
+    pub pos: CoordsXy,
+    pub vel: CoordsXy,
+}
+
+impl Point {
+    fn mov(&mut self) {
+        self.pos = (self.pos.0 + self.vel.0, self.pos.1 + self.vel.1);
+    }
+}
+
+fn positions(points: &[Point]) -> HashSet<CoordsXy> {
+    points.iter().map(|x| x.pos).collect()
+}
+
+fn is_writing(positions: &HashSet<CoordsXy>) -> bool {
+    let min_y = *positions.iter().map(|(_, y)| y).min().unwrap();
+    let max_y = *positions.iter().map(|(_, y)| y).max().unwrap();
+    max_y - min_y == 9
+}
+
+fn print(positions: &HashSet<CoordsXy>) -> String {
+    // get top left corner
+    let min_x = *positions.iter().map(|(x, _)| x).min().unwrap();
+    let min_y = *positions.iter().map(|(_, y)| y).min().unwrap();
+
+    // get bottom right corner
+    let max_x = *positions.iter().map(|(x, _)| x).max().unwrap();
+    let max_y = *positions.iter().map(|(_, y)| y).max().unwrap();
+
+    let mut s = String::new();
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            if positions.contains(&(x, y)) {
+                s += "#";
+            } else {
+                s += " ";
+            }
+        }
+        if y != max_y {
+            s += "\n"
+        }
+    }
+    s
+}
+
+fn iterate(mut points: Vec<Point>) -> (String, usize) {
+    let mut iters = 0;
+    while !is_writing(&positions(&points)) {
+        iters += 1;
+        for p in points.iter_mut() {
+            p.mov();
+        }
+    }
+    (print(&positions(&points)), iters)
+}
+
+fn parse_input(input: &str) -> Vec<Point> {
+    let mut points = vec![];
+    for line in input.lines() {
+        let s: Vec<_> = line.split(&['<', '>'][..]).collect();
+        let ps: Vec<_> = s[1].split(',').collect();
+        let vs: Vec<_> = s[3].split(',').collect();
+        let pos = (
+            ps[0].trim().parse::<isize>().unwrap(),
+            ps[1].trim().parse::<isize>().unwrap(),
+        );
+        let vel = (
+            vs[0].trim().parse::<isize>().unwrap(),
+            vs[1].trim().parse::<isize>().unwrap(),
+        );
+        points.push(Point { pos, vel });
+    }
+    points
+}
 
 pub struct Day10 {}
 
 impl Day for Day10 {
-    fn star1(&self, _input: &str) -> String {
-        String::from("not implemented")
+    fn star1(&self, input: &str) -> String {
+        let points = parse_input(input);
+        let (res, _) = iterate(points);
+        res
     }
 
-    fn star2(&self, _input: &str) -> String {
-        String::from("not implemented")
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn ex1() {
-        let d = Day10 {};
-        assert_eq!(d.star1(""), "not implemented");
+    fn star2(&self, input: &str) -> String {
+        let points = parse_input(input);
+        let (_, secs) = iterate(points);
+        format!("{}", secs)
     }
 }
---
+++ year2018/src/day12.rs
@@ -0,0 +1,127 @@
+use common::day::Day;
+use std::collections::BTreeSet;
+use std::collections::HashMap;
+
+pub struct Day12 {}
+
+type NeighborConfig = [bool; 5];
+type Rules = HashMap<NeighborConfig, bool>;
+type Gen = BTreeSet<isize>;
+
+fn next_val(pos: isize, last_gen: &Gen, rules: &Rules) -> bool {
+    let mut config = [false; 5];
+    for i in -2..=2 {
+        config[(i + 2) as usize] = last_gen.contains(&(pos + i));
+    }
+    *rules.get(&config).unwrap()
+}
+
+fn next_gen(last_gen: &Gen, rules: &Rules) -> Gen {
+    let mut next_gen = BTreeSet::new();
+    let min_pos = last_gen.iter().next().unwrap() - 2;
+    let max_pos = last_gen.iter().rev().next().unwrap() + 2;
+    for pos in min_pos..=max_pos {
+        if next_val(pos, last_gen, rules) {
+            next_gen.insert(pos);
+        }
+    }
+    next_gen
+}
+
+fn parse_input(input: &str) -> (Gen, Rules) {
+    let mut it_parts = input.split("\n\n");
+
+    // initial state
+    let isline = it_parts.next().unwrap();
+    let isline_parts: Vec<_> = isline.split_whitespace().collect();
+    let mut init_gen = BTreeSet::new();
+    for (i, c) in isline_parts[2].chars().enumerate() {
+        if c == '#' {
+            init_gen.insert(i as isize);
+        }
+    }
+
+    // rules
+    let rules_lines = it_parts.next().unwrap();
+    let mut rules = HashMap::new();
+    for line in rules_lines.lines() {
+        let line_parts: Vec<_> = line.split_whitespace().collect();
+        let mut config = [false; 5];
+        for (i, item) in config.iter_mut().enumerate() {
+            *item = line_parts[0].chars().nth(i).unwrap() == '#';
+        }
+        rules.insert(config, line_parts[2].starts_with('#'));
+    }
+
+    (init_gen, rules)
+}
+
+impl Day for Day12 {
+    fn star1(&self, input: &str) -> String {
+        let (mut gen, rules) = parse_input(input);
+        for _ in 0..20 {
+            gen = next_gen(&gen, &rules);
+        }
+        format!("{}", gen.iter().sum::<isize>())
+    }
+
+    fn star2(&self, input: &str) -> String {
+        let (mut gen, rules) = parse_input(input);
+
+        let mut sum_1000 = 0;
+        for i in 1..=2000 {
+            gen = next_gen(&gen, &rules);
+            if i == 1000 {
+                sum_1000 = gen.iter().sum::<isize>();
+            }
+        }
+        let sum_2000 = gen.iter().sum::<isize>();
+        format!("{}", sum_1000 + (sum_2000 - sum_1000) * (49_999_999))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        let input = r#"initial state: #..#.#..##......###...###
+
+##### => .
+####. => #
+###.# => #
+###.. => #
+##.## => #
+##.#. => #
+##..# => .
+##... => .
+#.### => #
+#.##. => .
+#.#.# => #
+#.#.. => .
+#..## => .
+#..#. => .
+#...# => .
+#.... => .
+.#### => #
+.###. => .
+.##.# => .
+.##.. => #
+.#.## => #
+.#.#. => #
+.#..# => .
+.#... => #
+..### => .
+..##. => .
+..#.# => .
+..#.. => #
+...## => #
+...#. => .
+....# => .
+..... => ."#;
+
+        let d = Day12 {};
+        assert_eq!(d.star1(input), "325");
+    }
+}
