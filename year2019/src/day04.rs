1415909d75221aee7f362033e67dfd8ab28bbfe8
Peter Helbing <peter@abulafia.org>
2021-12-12 20:11:45 +0100

    Year 2019, Day 04

---
+++ year2019/src/day04.rs
@@ -0,0 +1,83 @@
+use common::day::Day;
+
+pub struct Day04 {}
+
+fn digits(n: &u32) -> Vec<u32> {
+    n.to_string()
+        .chars()
+        .filter_map(|x| x.to_digit(10))
+        .collect()
+}
+
+fn check_adj2(ds: &[u32]) -> bool {
+    let mut len = 1;
+    for d in ds.windows(2) {
+        if d[0] == d[1] {
+            len += 1;
+        } else {
+            if len == 2 {
+                return true;
+            }
+            len = 1;
+        }
+    }
+    len == 2
+}
+
+fn is_valid_pt1(n: &u32) -> bool {
+    let ds = digits(n);
+    let check_adj = ds.windows(2).any(|d| d[0] == d[1]);
+    let check_asc = ds.windows(2).all(|d| d[1] >= d[0]);
+    check_adj && check_asc
+}
+
+fn is_valid_pt2(n: &u32) -> bool {
+    let ds = digits(n);
+    let check_asc = ds.windows(2).all(|d| d[1] >= d[0]);
+    check_adj2(&ds) && check_asc
+}
+
+impl Day for Day04 {
+    fn star1(&self, input: &str) -> String {
+        let range: Vec<_> = input
+            .lines()
+            .next()
+            .unwrap()
+            .split('-')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        let count = (range[0]..=range[1]).filter(is_valid_pt1).count();
+        format!("{}", count)
+    }
+
+    fn star2(&self, input: &str) -> String {
+        let range: Vec<_> = input
+            .lines()
+            .next()
+            .unwrap()
+            .split('-')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        let count = (range[0]..=range[1]).filter(is_valid_pt2).count();
+        format!("{}", count)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn star1() {
+        assert_eq!(is_valid_pt1(&111111), true);
+        assert_eq!(is_valid_pt1(&223450), false);
+        assert_eq!(is_valid_pt1(&123789), false);
+    }
+
+    #[test]
+    fn star2() {
+        assert_eq!(is_valid_pt2(&112233), true);
+        assert_eq!(is_valid_pt2(&123444), false);
+        assert_eq!(is_valid_pt2(&111122), true);
+    }
+}
