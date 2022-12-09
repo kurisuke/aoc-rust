05b0768d865e62dbc03a09777ce9113c90254e1c
Peter Helbing <peter@abulafia.org>
2022-12-09 09:28:15 +0100

    Year 2022, Day 09: optimize & refactor

--- year2022/src/day09.rs
+++ year2022/src/day09.rs
@@ -1,22 +1,10 @@
 use common::day::Day;
 use std::collections::HashSet;
-use std::ops::{Add, Sub};
 
 pub struct Day09 {}
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 struct Pos(i64, i64);
-
-const DIRS: [Pos; 8] = [
-    Pos(-1, -1),
-    Pos(0, -1),
-    Pos(1, -1),
-    Pos(-1, 0),
-    Pos(1, 0),
-    Pos(-1, 1),
-    Pos(0, 1),
-    Pos(1, 1),
-];
 
 impl Pos {
     fn moves(&self, other: &Pos) -> u64 {
@@ -24,94 +12,64 @@ impl Pos {
             .unsigned_abs()
             .max((self.1 - other.1).unsigned_abs())
     }
-
-    fn manhattan(&self, other: &Pos) -> u64 {
-        (self.0 - other.0).unsigned_abs() + (self.1 - other.1).unsigned_abs()
-    }
-}
-
-impl Add for Pos {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self::Output {
-        Self(self.0 + other.0, self.1 + other.1)
-    }
-}
-
-impl Sub for Pos {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self::Output {
-        Self(self.0 - other.0, self.1 - other.1)
-    }
-}
-
-fn move_tail(head: &Pos, tail: &Pos) -> Pos {
-    let delta_moves = head.moves(tail);
-    if delta_moves > 1 {
-        DIRS.iter()
-            .map(|d| *tail + *d)
-            .min_by(|t1, t2| head.manhattan(t1).cmp(&head.manhattan(t2)))
-            .unwrap()
-    } else {
-        *tail
-    }
 }
 
 struct MoveCmd {
-    dir: Pos,
+    dir: char,
     repeat: usize,
 }
 
 fn parse_input(input: &str) -> impl Iterator<Item = MoveCmd> + '_ {
     input.lines().map(|line| {
         let mut tokens = line.split_whitespace();
-        let dir = match tokens.next().unwrap() {
-            "L" => Pos(-1, 0),
-            "R" => Pos(1, 0),
-            "U" => Pos(0, -1),
-            "D" => Pos(0, 1),
-            _ => unreachable!(),
-        };
+        let dir = tokens.next().unwrap().chars().next().unwrap();
         let repeat = tokens.next().unwrap().parse().unwrap();
         MoveCmd { dir, repeat }
     })
 }
 
+fn run(input: &str, len: usize) -> usize {
+    let cmds = parse_input(input);
+    let mut knots = vec![Pos(0, 0); len];
+    let mut tail_pos = HashSet::new();
+
+    for cmd in cmds {
+        for _ in 0..cmd.repeat {
+            match cmd.dir {
+                'L' => {
+                    knots[0].0 -= 1;
+                }
+                'R' => {
+                    knots[0].0 += 1;
+                }
+                'U' => {
+                    knots[0].1 -= 1;
+                }
+                'D' => {
+                    knots[0].1 += 1;
+                }
+                _ => unreachable!(),
+            }
+            for i in 1..len {
+                let delta_moves = knots[i - 1].moves(&knots[i]);
+                if delta_moves > 1 {
+                    knots[i].0 += (knots[i - 1].0 - knots[i].0).signum();
+                    knots[i].1 += (knots[i - 1].1 - knots[i].1).signum();
+                }
+            }
+            tail_pos.insert(knots[len - 1]);
+        }
+    }
+    tail_pos.len()
+}
+
 impl Day for Day09 {
     fn star1(&self, input: &str) -> String {
-        let cmds = parse_input(input);
-        let mut head = Pos(0, 0);
-        let mut tail = Pos(0, 0);
-        let mut tail_pos = HashSet::new();
-
-        for cmd in cmds {
-            for _ in 0..cmd.repeat {
-                head = head + cmd.dir;
-                tail = move_tail(&head, &tail);
-                tail_pos.insert(tail);
-            }
-        }
-
-        format!("{}", tail_pos.len())
+        format!("{}", run(input, 2))
     }
 
     fn star2(&self, input: &str) -> String {
-        let cmds = parse_input(input);
-        let mut knots = [Pos(0, 0); 10];
-        let mut tail_pos = HashSet::new();
-
-        for cmd in cmds {
-            for _ in 0..cmd.repeat {
-                knots[0] = knots[0] + cmd.dir;
-                for i in 1..=9 {
-                    knots[i] = move_tail(&knots[i - 1], &knots[i]);
-                }
-                tail_pos.insert(knots[9]);
-            }
-        }
-
-        format!("{}", tail_pos.len())
+        format!("{}", run(input, 10))
     }
 }
 
