b8ddb9c0ae1ed7e56e3eca0151ccb2a8a7d285e5
Peter Helbing <peter@abulafia.org>
2023-12-16 16:52:40 +0100

    Year 2023, Day 14, part 2

--- year2023/src/day14.rs
+++ year2023/src/day14.rs
@@ -1,36 +1,112 @@
+use std::collections::HashMap;
+
 use common::day::Day;
-use util::grid2d::Grid2D;
+use util::grid2d::{Direction, Grid2D};
 
 pub struct Day14 {}
 
 impl Day for Day14 {
     fn star1(&self, input: &str) -> String {
         let mut grid = Grid2D::new(input).unwrap();
-        slide_north(&mut grid);
+        slide_direction(&mut grid, Direction::N);
         load(&grid).to_string()
     }
 
-    fn star2(&self, _input: &str) -> String {
-        String::from("not implemented")
+    fn star2(&self, input: &str) -> String {
+        let mut grid = Grid2D::new(input).unwrap();
+        let mut loads = HashMap::new();
+        let mut i = 0;
+        loads.insert(grid.clone(), 0);
+        loop {
+            i += 1;
+            slide_cycle(&mut grid);
+            if loads.contains_key(&grid) {
+                let offset = loads.get(&grid).unwrap();
+                let length = i - loads.get(&grid).unwrap();
+                let target = 1000000000;
+                let target_index = offset + (target - offset) % length;
+                return load(
+                    loads
+                        .iter()
+                        .find(|(_, x)| x == &&target_index)
+                        .map(|(grid, _)| grid)
+                        .unwrap(),
+                )
+                .to_string();
+            } else {
+                loads.insert(grid.clone(), i);
+            }
+        }
     }
 }
 
-fn slide_north(grid: &mut Grid2D<char>) {
-    for i in 0..grid.width() {
-        grid.set_col(i, slide_col_north(grid.col(i).unwrap()));
+fn slide_cycle(grid: &mut Grid2D<char>) {
+    slide_direction(grid, Direction::N);
+    slide_direction(grid, Direction::W);
+    slide_direction(grid, Direction::S);
+    slide_direction(grid, Direction::E);
+}
+
+fn slide_direction(grid: &mut Grid2D<char>, dir: Direction) {
+    match dir {
+        Direction::N => {
+            for i in 0..grid.width() {
+                grid.set_col(i, slide_col(grid.col(i).unwrap(), true));
+            }
+        }
+        Direction::S => {
+            for i in 0..grid.width() {
+                grid.set_col(i, slide_col(grid.col(i).unwrap(), false));
+            }
+        }
+        Direction::W => {
+            for i in 0..grid.height() {
+                grid.set_row(i, slide_row(grid.row(i).unwrap(), true));
+            }
+        }
+        Direction::E => {
+            for i in 0..grid.width() {
+                grid.set_row(i, slide_row(grid.row(i).unwrap(), false));
+            }
+        }
+        _ => unreachable!(),
     }
 }
 
-fn slide_col_north(col: Vec<&char>) -> Vec<char> {
+fn slide_col(col: Vec<&char>, north: bool) -> Vec<char> {
     let mut col_new = vec![];
     for range in col.split(|x| x == &&'#') {
-        let num_movable = range.into_iter().filter(|x| x == &&&'O').count();
-        col_new.extend(std::iter::repeat('O').take(num_movable));
-        col_new.extend(std::iter::repeat('.').take(range.len() - num_movable));
-        col_new.push('#');
+        let num_movable = range.iter().filter(|x| x == &&&'O').count();
+        if north {
+            col_new.extend(std::iter::repeat('O').take(num_movable));
+            col_new.extend(std::iter::repeat('.').take(range.len() - num_movable));
+            col_new.push('#');
+        } else {
+            col_new.extend(std::iter::repeat('.').take(range.len() - num_movable));
+            col_new.extend(std::iter::repeat('O').take(num_movable));
+            col_new.push('#');
+        }
     }
     col_new.pop();
     col_new
+}
+
+fn slide_row(row: Vec<&char>, west: bool) -> Vec<char> {
+    let mut row_new = vec![];
+    for range in row.split(|x| x == &&'#') {
+        let num_movable = range.iter().filter(|x| x == &&&'O').count();
+        if west {
+            row_new.extend(std::iter::repeat('O').take(num_movable));
+            row_new.extend(std::iter::repeat('.').take(range.len() - num_movable));
+            row_new.push('#');
+        } else {
+            row_new.extend(std::iter::repeat('.').take(range.len() - num_movable));
+            row_new.extend(std::iter::repeat('O').take(num_movable));
+            row_new.push('#');
+        }
+    }
+    row_new.pop();
+    row_new
 }
 
 fn load(grid: &Grid2D<char>) -> usize {
@@ -65,5 +141,11 @@ O.#..O.#.#
     fn ex1() {
         let d = Day14 {};
         assert_eq!(d.star1(INPUT), "136");
+    }
+
+    #[test]
+    fn ex2() {
+        let d = Day14 {};
+        assert_eq!(d.star2(INPUT), "64");
     }
 }
