1a3c155fbee23907f50a11a7f5c68b81871bc49f
Peter Helbing <github@ph157.de>
2024-12-27 12:16:18 +0100

    Year 2024, Day 6: optimize

--- year2024/src/day06.rs
+++ year2024/src/day06.rs
@@ -7,26 +7,30 @@ pub struct Day06 {}
 
 impl Day for Day06 {
     fn star1(&self, input: &str) -> String {
-        let grid = Grid2D::new(input).unwrap();
-        let visited = walk(grid).unwrap();
-        let visited: HashSet<_> = visited.into_iter().map(|g| g.pos).collect();
-        format!("{}", visited.len())
+        let mut grid = Grid2D::new(input).unwrap();
+        let start_pos = grid.find('^').unwrap();
+        grid.set(&start_pos, '.');
+
+        walk(&grid, start_pos, None, true)
+            .unwrap()
+            .unwrap()
+            .len()
+            .to_string()
     }
 
     fn star2(&self, input: &str) -> String {
-        let grid = Grid2D::new(input).unwrap();
+        let mut grid = Grid2D::new(input).unwrap();
+        let start_pos = grid.find('^').unwrap();
+        grid.set(&start_pos, '.');
 
         // when adding an obstacle, only consider positions the guard would actually visit
         // on their normal walk, but ignore their start position
-        let visited = walk(grid.clone()).unwrap();
-        let mut visited_pos: HashSet<_> = visited.into_iter().map(|g| g.pos).collect();
-        visited_pos.remove(&grid.find('^').unwrap());
+        let mut visited_pos = walk(&grid, start_pos, None, true).unwrap().unwrap();
+        visited_pos.remove(&start_pos);
 
         let mut num_loop_pos = 0;
         for add_pos in visited_pos {
-            let mut grid_add = grid.clone();
-            grid_add.set(&add_pos, '#');
-            if walk(grid_add).is_err() {
+            if walk(&grid, start_pos, Some(add_pos), false).is_err() {
                 num_loop_pos += 1;
             }
         }
@@ -40,20 +44,52 @@ struct Guard {
     dir: Direction,
 }
 
-fn walk(mut grid: Grid2D<char>) -> Result<HashSet<Guard>, Guard> {
-    let pos = grid.find('^').unwrap();
+impl Guard {
+    fn id(&self) -> usize {
+        self.pos.x as usize
+            + ((self.pos.y as usize) << 8)
+            + (match self.dir {
+                Direction::N => 0,
+                Direction::E => 1,
+                Direction::S => 2,
+                Direction::W => 3,
+                _ => unreachable!(),
+            } << 16)
+    }
+}
+
+fn walk(
+    grid: &Grid2D<char>,
+    pos: Coords,
+    obstacle: Option<Coords>,
+    count_pos: bool,
+) -> Result<Option<HashSet<Coords>>, ()> {
     let mut guard = Guard {
         pos,
         dir: Direction::N,
     };
 
-    let mut visited = HashSet::new();
-    visited.insert(guard);
-    grid.set(&pos, '.');
+    let mut visited = vec![false; 1 << 18];
+    visited[guard.id()] = true;
+    let mut positions = HashSet::new();
+    if count_pos {
+        positions.insert(guard.pos);
+    }
 
     loop {
         let check_pos = guard.pos.mov(guard.dir);
         if let Some(v) = grid.at(&check_pos) {
+            if obstacle.is_some() && check_pos == obstacle.unwrap() {
+                guard.dir = match guard.dir {
+                    Direction::N => Direction::E,
+                    Direction::E => Direction::S,
+                    Direction::S => Direction::W,
+                    Direction::W => Direction::N,
+                    _ => unreachable!(),
+                };
+                continue;
+            }
+
             match v {
                 '#' => {
                     guard.dir = match guard.dir {
@@ -66,8 +102,14 @@ fn walk(mut grid: Grid2D<char>) -> Result<HashSet<Guard>, Guard> {
                 }
                 '.' => {
                     guard.pos = check_pos;
-                    if !visited.insert(guard) {
-                        return Err(guard);
+                    if count_pos {
+                        positions.insert(guard.pos);
+                    }
+
+                    if visited[guard.id()] {
+                        return Err(());
+                    } else {
+                        visited[guard.id()] = true;
                     }
                 }
                 _ => unreachable!(),
@@ -77,7 +119,11 @@ fn walk(mut grid: Grid2D<char>) -> Result<HashSet<Guard>, Guard> {
         }
     }
 
-    Ok(visited)
+    if count_pos {
+        Ok(Some(positions))
+    } else {
+        Ok(None)
+    }
 }
 
 #[cfg(test)]
